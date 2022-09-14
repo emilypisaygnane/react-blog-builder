e374a7e682639c30149b834fd6540a223e02dc65
Emily Pisaygnane <episaygnane@me.com>
2022-09-14 12:19:07 -0700

    set state for title, and passed the title as prop to pres comp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useState } from 'react';
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
 
@@ -7,12 +7,19 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-
+  const [title, setTitle] = useState('default state');
+  // const [subtitle, setSubtitle] = useState('default state');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview 
+        title={title} 
+        // subtitle={subtitle} 
+      />
+      <Editor 
+        title={title} setTitle={setTitle} 
+        // subtitle={subtitle} set
+      />
     </main>
   );
 }
